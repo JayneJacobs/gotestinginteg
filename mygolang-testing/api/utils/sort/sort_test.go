90625dfffb52d719843c4b73817e7bc5f5c22126
jjacob151 <jayne_jacobs@cable.comcast.com>
2019-11-18 09:29:15 -0500

    adding asserts

--- mygolang-testing/api/utils/sort/sort_test.go
+++ mygolang-testing/api/utils/sort/sort_test.go
@@ -2,20 +2,54 @@ package sort
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"gotrain/golang-testing/src/api/services"
 	"testing"
+	"time"
 )
 
 func TestBubbleSort(t *testing.T) {
 
-	elements := []int{9, 8, 7, 5, 4, 6, 1, 0, 3, 2}
-	fmt.Println(elements)
-
-	BubbleSort(elements)
+	elements := []int{9, 8, 7, 5, 4, 6, 1, 2, 3, 0}
+	felement := elements[0]
 	lelement := elements[len(elements)-1]
+	// if len(elements) != 10 {
+	// 	t.Error("slice should contain 10 elements")
+	// }
+
+	assert.EqualValues(t, 10, len(elements))
+
+	timeoutChan := make(chan bool, 1)
+
+	go func() {
+		BubbleSort(elements)
+		timeoutChan <- false
+	}()
+
+	go func() {
+		time.Sleep(500 * time.Millisecond)
+		timeoutChan <- true
+	}()
+
+	if <-timeoutChan {
+		assert.Fail(t, "bubble sort took more than 500 ms")
+		return
+	}
+
+	fmt.Println(elements)
+	// if elements[len(elements)-1] != 0 {
+	// 	t.Error("The lastElement should be 0. it is ", lelement)
+	// }
+	assert.EqualValues(t, lelement, 0)
+	// if elements[0] != 9 {
+	// 	t.Error("The first element should be 9, it is ", felement)
+	// }
+	assert.EqualValues(t, felement, 9)
+
 	if elements[len(elements)-1] != 0 {
 		t.Error("The lastElement should be 0. it is ", lelement)
 	}
-	felement := elements[0]
+
 	if elements[0] != 9 {
 		t.Error("The first element should be 9, it is ", felement)
 	}
@@ -53,4 +87,11 @@ func TestSort(t *testing.T) {
 		t.Error("The first element should be 0, it is ", felement)
 	}
 	fmt.Println(elements)
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	elements := services.GetElements(10)
+	for i := 0; i < b.N; i++ {
+		BubbleSort(elements)
+	}
 }
